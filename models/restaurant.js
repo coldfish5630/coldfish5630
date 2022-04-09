c1416ccb09f874405168b4925762801fa54ccb28
cuisine <layan.schicksal@gmail.com>
2022-04-10 05:13:16 +0900

    feat: add restaurant seeder

---
+++ models/restaurant.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const restaurantSchema = new Schema({
+  _id: Number,
+  name: {
+    type: String,
+    require: true
+  },
+  name_en: String,
+  category: {
+    type: String,
+    require: true
+  },
+  image: {
+    type: String,
+    require: true
+  },
+  location: {
+    type: String,
+    require: true
+  },
+  phone: {
+    type: String,
+    require: true
+  },
+  google_map: {
+    type: String,
+    require: true
+  },
+  rating: {
+    type: Number,
+    require: true
+  },
+  description: {
+    type: String,
+    require: true
+  }
+})
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
