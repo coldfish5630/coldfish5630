a55d6eeb81551f06846819a8384999d74d67ff1e
cuisine <layan.schicksal@gmail.com>
2022-04-11 01:03:24 +0900

    feat: add create function & detail page

--- app.js
+++ app.js
@@ -1,40 +1,90 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Restaurant = require('./models/restaurant')
+require('dotenv').config()
+
 const app = express()
 
 const port = 3000
 
-const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json').results
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
+const exphbs = require('express-handlebars')
+const restaurant = require('./models/restaurant')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurantList })
+  Restaurant.find()
+    .lean()
+    .then(restaurant => {
+      res.render('index', { restaurant })
+    })
+    .catch(error => console.error(error))
 })
 
 app.get('/restaurants/:id', (req, res) => {
-  const indexRestaurant = restaurantList.find(
-    restaurant => restaurant.id.toString() === req.params.id
-  )
-  res.render('show', { indexRestaurant })
+  const id = req.params.id
+  Restaurant.findById(id)
+    .lean()
+    .then(restaurant => {
+      res.render('show', { restaurant })
+    })
+    .catch(error => console.error(error))
 })
 
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.trim().toLowerCase()
-
-  const filterRestaurant = restaurantList.filter(
-    restaurant =>
-      restaurant.name.toLowerCase().includes(keyword) ||
-      restaurant.name_en.toLowerCase().includes(keyword) ||
-      restaurant.category.includes(req.query.keyword.trim())
-  )
-  res.render('index', {
-    restaurantList: filterRestaurant,
-    keyword
+  const keyword = req.query.keyword.trim()
+  return Restaurant.find({
+    $or: [
+      { name: { $regex: `${keyword}`, $options: 'i' } },
+      { name_en: { $regex: `${keyword}`, $options: 'i' } },
+      { category: { $regex: `${keyword}`, $options: 'i' } }
+    ]
   })
+    .lean()
+    .then(restaurant => {
+      res.render('index', { restaurant, keyword })
+    })
+    .catch(error => console.error(error))
+})
+
+app.get('/restaurant/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/restaurant', (req, res) => {
+  const info = req.body
+  return Restaurant.create({
+    name: info.name,
+    name_en: info.enName,
+    category: info.category,
+    location: info.location,
+    google_map: info.googleMap,
+    phone: info.phone,
+    rating: info.rating,
+    description: info.description,
+    image: info.image
+  })
+    .then(res.redirect('/'))
+    .catch(error => console.error(error))
 })
 
 app.listen(port, () => {
--- models/restaurant.js
+++ models/restaurant.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const restaurantSchema = new Schema({
-  _id: Number,
+  id: Number,
   name: {
     type: String,
     require: true
