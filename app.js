5d3ce97ec2ceacb82ad5cf32dcb11d168939eff3
coldfish5630 <layan.schicksal@gmail.com>
2022-06-12 20:55:12 +0900

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
+const session = require('express-session')
 const Restaurant = require('./models/restaurant')
 const methodOverride = require('method-override')
 const routes = require('./routes')
@@ -15,6 +16,13 @@ const exphbs = require('express-handlebars')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+)
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
